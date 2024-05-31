7f63ebc525f000464a5e5cee9abb39a9fea235a0
Superbigboi <realkranda@gmail.com>
2024-05-31 15:47:56 +0100

    Version 2 - Friday, May 31st, 2024
    i'm on the module 3 module test

---
+++ Module 3/Section 3/Module 3 Section 2 Section Practice.js
@@ -0,0 +1,5 @@
+let width = prompt("What do you want your width to be?");
+let height = prompt("What do you want your height to be?");
+let length = prompt("What do you want your length to be?");
+let volume = width*height*length;
+alert(`your volume is ${volume}`);
