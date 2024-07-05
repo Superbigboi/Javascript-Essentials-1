da60b85a6ba6a76f526025ec82f4ded84eefa5c1
Superbigboi <realkranda@gmail.com>
2024-07-05 13:59:51 +0100

    Friday, July 4th, 2024
    i'm on Module 5 Section 2 Exercise One Task 1A

---
+++ Module 5/Section 2 - Functions - Part 2/Module 5 Section 2 Section Practice Excercise 1.js
@@ -0,0 +1,15 @@
+let numbers = [50, 10, 40, 30, 20];
+let compareNumbers = (a, b) => {
+     let retVal = 0;
+     if (a < b) retVal = -1;
+     else retVal = 1;
+     return retVal;
+}
+let sorted = numbers.sort(compareNumbers);
+console.log(sorted); // [10, 20, 30, 40, 50]
+
+
+// use an anonymous function;
+// use an arrow function;
+// consider skipping the if statement.
+// 
