a46233ec9c3b58d7eae58d26b5b9efa29f0d09fe
Superbigboi <realkranda@gmail.com>
2024-08-01 16:33:09 +0100

    Thursday, August 1st, 2024
    im on the Module 5 Section 2 LAB part 2

--- Module 5/Section 2 - Functions - Part 2/Module 5 Section 2 Section LAB part 2.js
+++ Module 5/Section 2 - Functions - Part 2/Module 5 Section 2 Section LAB part 2.js
@@ -12,43 +12,103 @@ let contacts = [{
     email: "libero@convallis.edu"
 }];
 
-let showContact = (contacts,index,sort) =>{
-    let order;
-    if(sort == "name"){contacts.sort(sort)}
-    if(sort =="phone"){}
-    if(sort == "email"){}
 
-    if(contacts instanceof Array ){
-    // if(sort == "name"){order = ["name", "phone", "email"];}
-    // else if(sort =="phone"){order = ["phone", "name", "email"];}
-    // else if(sort == "email"){order = ["email", "name", "phone"];}
-        console.log(`${contacts[index].order[0]}/${contacts[index].order[1]}/${contacts[index].order[32]}`);
+    let showContact = (contacts,index) =>{
+        if(contacts instanceof Array && contacts[index]){
+            // console.log(true);
+            console.log(`${contacts[index].name}/${contacts[index].phone}/${contacts[index].email}`);
+            alert(`${contacts[index].name}/${contacts[index].phone}/${contacts[index].email}`);
+            // console.log(contacts[1])
+        }
+        else{
+            console.log("bad input");
+        }
+    };
+    function showAllContacts(tc){
+        if(tc instanceof Array){
+               for(number of contacts){
+                // console.log(number)
+                console.log(`${number.name} / ${number.phone} / ${number.email}`);
+                alert(`${number.name} / ${number.phone} / ${number.email}`);
+    
+                // console.log(number)
+            // console.log(`${contacts[number].name}/${contacts[number].phone}/${contacts[number].phone}`);
+        }
+       }
+       else{console.log('input contacts')}
     }
-    else{
-        console.log("wrong input");
+    function addNewContact(contacts, name, phone, email){
+        if(contacts instanceof Array && name && phone && email){
+                contacts.push({name:name, phone:phone, email:email});
+                            console.log("contact added successfully")
+            
+        }else{(console.log("contact adding unsuccessful"))}
     }
+
+
+
+
+// function sortType(t){
+//     if (t=="name"){sName()}
+//     else if (t=="phone"){}
+//     else if (t=="email"){}
+//     else{return "input name, phone, or email"}
+// }
+function sName(){
+    
+}
+function sPhone(){
 
 }
-function showAllContacts(tc){
-    if(tc instanceof Array){
-           for(let number in contacts){
-            console.log(number)
-        console.log(`${contacts[number].name}/${contacts[number].phone}/${contacts[number].phone}`);
+function sEmail(a,b){
+    numbers.sort((a, b) => a - b);
+}
 
+
+let end = false;
+
+do{
+    let choice = prompt("what do you want to display? (first, last, all, new or quit)?");
+    switch(choice){
+        case "first":
+            showContact(contacts,0);
+            break;
+        case "last":
+            showContact(contacts,contacts.length-1);
+            break;
+        case "all":
+            showAllContacts(contacts);
+            break;
+        case "new":
+            let name = prompt("Enter name:, Michael");
+            let phone = prompt("Enter phone:, 090 861 9090");
+            let email = prompt("Enter email:, email@gmail.com");
+            addNewContact(contacts, name, phone, email);
+            break;
+        case "sort":
+            let type = prompt("do you want to sort by name, email, or phone?");
+            let wts = prompt("what do you want to sort?");
+            if (type=="name"){sName()} 
+            if (type=="email"){sEmail()} 
+            if (type=="phone"){sPhone()} 
+            
+            // sortType(type)
+        case "quit":
+            end = true; 
+            break;    
+        default:
+            console.log("Invalid option");
+            break;
+     
     }
-   }
-   else console.log('input contacts')
 }
-function addNewContact(contacts, name, phone, email,sort){
-    if(contacts instanceof Array && name && phone && email){
-            contacts.push({name:name, phone:phone, email:email});
-                        console.log("contact added successfully");
-        
-    }else{(console.log("contact adding unsuccessful"))}
-}
+    
 
-console.log(showContact(contacts,1))
+while(end==false)
+
+
+
 // console.log(showAllContacts(contacts))
-// console.log(showContact(contacts,1));
-// console.log(showAllContacts(contacts));
-// console.log(addNewContact(contacts,'Michael',090,'realkranda@gmail.com'));
+// console.log(showContact(contacts,0));
+// console.log(addNewContact(contacts,'Michael',090,'realkranda@gmail.com'))
+// console.log(showAllContacts(contacts))
--- Module 5/Section 2 - Functions - Part 2/Module 5 Section 2 Section LAB.js
+++ Module 5/Section 2 - Functions - Part 2/Module 5 Section 2 Section LAB.js
@@ -13,40 +13,76 @@ let contacts = [{
 }];
 
 
-let choice = prompt("what do you want to display? (first, last, all, new or quit)?");
+    let showContact = (contacts,index) =>{
+        if(contacts instanceof Array && contacts[index]){
+            // console.log(true);
+            console.log(`${contacts[index].name}/${contacts[index].phone}/${contacts[index].email}`);
+            alert(`${contacts[index].name}/${contacts[index].phone}/${contacts[index].email}`);
+            // console.log(contacts[1])
+        }
+        else{
+            console.log("bad input");
+        }
+    };
+    function showAllContacts(tc){
+        if(tc instanceof Array){
+               for(number of contacts){
+                // console.log(number)
+                console.log(`${number.name} / ${number.phone} / ${number.email}`);
+                alert(`${number.name} / ${number.phone} / ${number.email}`);
+    
+                // console.log(number)
+            // console.log(`${contacts[number].name}/${contacts[number].phone}/${contacts[number].phone}`);
+        }
+       }
+       else{console.log('input contacts')}
+    }
+    function addNewContact(contacts, name, phone, email){
+        if(contacts instanceof Array && name && phone && email){
+                contacts.push({name:name, phone:phone, email:email});
+                            console.log("contact added successfully")
+            
+        }else{(console.log("contact adding unsuccessful"))}
+    }
 
-let showContact = (contacts,index) =>{
-    if(contacts instanceof Array && contacts[index]){
-        // console.log(true);
-        console.log(`${contacts[index].name}/${contacts[index].phone}/${contacts[index].email}`);
-        // console.log(contacts[1])
-    }
-    else{
-        console.log("bad input");
-    }
-};
-function showAllContacts(tc){
-    if(tc instanceof Array){
-           for(number of contacts){
-            // console.log(number)
-            console.log(`${number.name} / ${number.phone} / ${number.email}`);
 
-            // console.log(number)
-        // console.log(`${contacts[number].name}/${contacts[number].phone}/${contacts[number].phone}`);
+
+let end = false;
+
+do{
+    let choice = prompt("what do you want to display? (first, last, all, new or quit)?");
+    switch(choice){
+        case "first":
+            showContact(contacts,0);
+            break;
+        case "last":
+            showContact(contacts,contacts.length-1);
+            break;
+        case "all":
+            showAllContacts(contacts);
+            break;
+        case "new":
+            let name = prompt("Enter name:, Michael");
+            let phone = prompt("Enter phone:, 090 861 9090");
+            let email = prompt("Enter email:, email@gmail.com");
+            addNewContact(contacts, name, phone, email);
+            break;
+        case "quit":
+            end = true; 
+            break;    
+        default:
+            console.log("Invalid option");
+            break;
+     
     }
-   }
-   else{console.log('input contacts')}
 }
-function addNewContact(contacts, name, phone, email){
-    if(contacts instanceof Array && name && phone && email){
-            contacts.push({name:name, phone:phone, email:email});
-                        console.log("contact added successfully")
-        
-    }else{(console.log("contact adding unsuccessful"))}
-}
+    
+
+while(end==false)
+
 
 
 // console.log(showAllContacts(contacts))
 // console.log(showContact(contacts,0));
-console.log(addNewContact(contacts,'Michael',090,'realkranda@gmail.com'))
-console.log(showAllContacts(contacts))
+// console.log(addNewContact(contacts,'Michael',090,'realkranda@gmail.com'))
+// console.log(showAllContacts(contacts))
