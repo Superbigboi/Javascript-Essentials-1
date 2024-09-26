02760712e42cb2e23bdb6b5d067b911ef581a628
Superbigboi <realkranda@gmail.com>
2024-09-26 14:49:31 +0100

    pre Thursday, September 26th, 2024
    i forgot to upload this last week

---
+++ Module 6/Section 3/Module 6 Section 3 Section Practice 3.js
@@ -0,0 +1,17 @@
+function max(array) { 
+    let maxValue = array[1]; 
+    for(num of array){
+        if(array[num] > maxValue) { 
+            maxValue = array[num]; 
+        } 
+    } 
+    return maxValue; 
+} 
+ 
+console.log(max([1, 4, 6, 2])); // -> 6 
+console.log(max([10, 4, 6, 2])); // -> 6
+
+
+// The function max should return the largest number from the array given as an argument. As you can see, in the second case it works 
+// incorrectly: instead of the value 10 we get 6. Using the debugger, trace the max function execution step by step. Observe the values of 
+// the i and maxValue variables. Locate the problem and fix the code so that it works correctly.
